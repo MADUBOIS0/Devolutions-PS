730cbd0e5041a22644f95a71884d3250b62cdd19
Marc-Antoine Dubois <marcantoine.dubois2001@gmail.com>
2024-04-23 13:33:24 -0400

    modified:   CyberArkGetCredential.ps1

--- CyberArkGetCredential.ps1
+++ CyberArkGetCredential.ps1
@@ -1,19 +1,33 @@
-#Fetch the Datasource
+#Goal: Showcase that it's possible to launch a CyberArk Dashboard from a PS Script.
+
+#Fetch the Datasource (Run this before calling other methods)
 Function AuthenticateRDM {
-    $ds = Get-RDMDataSource -Name "BreakGlass"
+    $ds = Get-RDMDataSource -Name "BreakGlass" #Change the RDM Datasource to the correct DS Name in RDM.
     Set-RDMCurrentDataSource $ds
 }
 
-Function GetCyberArk{
-    #Select the vault where CyberArk Dashboard is located
-    $Vault = Get-RDMRepository -Name "T-Dawgs"
-    Set-RDMCurrentRepository $Vault
+#Fetch the CyberArk Dashboard Entry information
+Function GetCyberArkEntry{
 
-    #Set the CyberArk entry name
+    #Vault where CyberArk Dashboard is located
+    $VaultName = "MarcVault"
+    #Name of the CyberArk DashBoard Entry
     $CyberArkEntryName = "CyberArk Vault - StandardUser"
 
-    #Fetch the entry
-    $Entry = Get-RDMSession | Where-Object {$_.Name -EQ $CyberArkEntryName}
+    #Select the vault where CyberArk Dashboard is located
+    $Vault = Get-RDMRepository -Name $VaultName
+    Set-RDMCurrentRepository $Vault
 
-    $T = '1'
+    #Get the information of the entry and return it
+    $Entry = Get-RDMEntry | Where-Object {$_.Name -EQ $CyberArkEntryName}
+    Return $Entry
+}
+
+
+#Open a CyberArk DashBoard
+Function OpenDashBoard{
+
+    #Get the CyberArk Dashboard by calling function GetCyberArkEntry
+    $DashBoardEntry = GetCyberArkEntry
+    Open-RDMSession -ID $DashBoardEntry.ID
 }
