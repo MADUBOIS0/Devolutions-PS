b635fdfe2e07ea4ce414c9145cd544ce1a879ad2
MADUBOIS0 <56685045+MADUBOIS0@users.noreply.github.com>
2025-11-13 10:54:03 -0500

    Add fields

--- Entry Oriented/GetAllEntriesByCustomFieldValueExport.ps1
+++ Entry Oriented/GetAllEntriesByCustomFieldValueExport.ps1
@@ -1,4 +1,5 @@
 Function GetAllEntriesByCustomFieldValueExport {
+    [CmdletBinding()]
     param (
         [string]$DataSourceName = "DVLS-02 AZ",
         [ValidateSet('CustomField1Value','CustomField2Value','CustomField3Value','CustomField4Value','CustomField5Value')]
@@ -15,6 +16,7 @@ Function GetAllEntriesByCustomFieldValueExport {
 
     $vaults = Get-RDMRepository
     $results = @()
+    $canRetrievePassword = $null -ne (Get-Command -Name Get-RDMSessionPassword -ErrorAction SilentlyContinue)
 
     foreach ($v in $vaults) {
         Set-RDMCurrentRepository -Repository $v
@@ -27,8 +29,45 @@ Function GetAllEntriesByCustomFieldValueExport {
                 continue
             }
 
-            $results += [pscustomobject]@{
+            $tagSource = $null
+            if ($s.PSObject.Properties.Name -contains 'TagList') {
+                $tagSource = $s.TagList
+            } elseif ($s.PSObject.Properties.Name -contains 'Tags') {
+                $tagSource = $s.Tags
+            }
+
+            if ($tagSource -is [string] -and [string]::IsNullOrWhiteSpace($tagSource)) {
+                $tagSource = $null
+            }
+
+            $tagValue = $null
+            if ($tagSource) {
+                if ($tagSource -is [System.Collections.IEnumerable] -and -not ($tagSource -is [string])) {
+                    $tagValue = ($tagSource | Where-Object { $_ } | ForEach-Object { $_.ToString().Trim() } | Where-Object { $_ } | Sort-Object -Unique) -join '; '
+                } else {
+                    $tagValue = $tagSource.ToString()
+                }
+            }
+
+            $password = ''
+            if ($canRetrievePassword) {
+                try {
+                    $retrievedPassword = Get-RDMSessionPassword -ID $s.ID -AsPlainText
+                    if ($null -ne $retrievedPassword) {
+                        $password = $retrievedPassword
+                    }
+                } catch {
+                    Write-Verbose "Unable to retrieve password for entry '$($s.Name)': $($_.Exception.Message)"
+                }
+            }
+
+            $results += [pscustomobject][ordered]@{
                 Name = $s.Name
+                'Entry Type' = $s.ConnectionType
+                Host = $s.Host
+                Folder = $s.Group
+                Tag = $tagValue
+                Password = $password
                 CustomFieldName = $CustomFieldProperty
                 CustomFieldValue = $customFieldValue
             }
@@ -41,7 +80,7 @@ Function GetAllEntriesByCustomFieldValueExport {
     }
 
     if ($results.Count -eq 0) {
-        "Name,CustomFieldName,CustomFieldValue" | Set-Content -Path $ExportPath -Encoding UTF8
+        "Name,""Entry Type"",Host,Folder,Tag,Password,CustomFieldName,CustomFieldValue" | Set-Content -Path $ExportPath -Encoding UTF8
         Write-Host "No sessions contained '$CustomFieldProperty'. Created empty export at $ExportPath."
         return
     }
